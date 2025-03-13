345ad10344ca9fd3f2d708096f9259509a285e1d
wutekk <wutek13@gmail.com>
2025-03-13 19:40:39 +0100

    Naprawa prędkości średniej

--- predkosc_srednia.py
+++ predkosc_srednia.py
@@ -5,22 +5,18 @@
 # v_sr = 2s / (s/v1 + s/v2)
 # v_sr = 2*v1*v2/((v2 + v1))
 ODL_MIAST = 1
-try:
+t = int(input())
+for i in range(t):
+    lista = list(map(int, input().split(" ")))
+    s1 = s2 = ODL_MIAST
+    v1 = lista[0]
+    v2 = lista[1]
 
-    t = int(input())
-    for i in range(t):
-        lista = list(map(int, input().split(" ")))
-        s1 = s2 = ODL_MIAST
-        v1 = lista[0]
-        v2 = lista[1]
-
-        t1 = s1 / v1 
-        t2 = s2 / v2
-        suma_s = s1 + s2 
-        suma_t = t1 + t2
-        wynik = suma_s / suma_t
-        wynik = round(wynik)
-        # suma drog / suma czasow = wynik
-        print(wynik)
-except EOFError:
-    pass
+    t1 = s1 / v1 
+    t2 = s2 / v2
+    suma_s = s1 + s2 
+    suma_t = t1 + t2
+    wynik = suma_s / suma_t
+    wynik = round(wynik)
+    # suma drog / suma czasow = wynik
+    print(wynik)
